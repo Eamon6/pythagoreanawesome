8ad71ed051ecf171d9e378bcb9fec607c615e6e0
Eamon6 <ed072708@wgcloud.org>
2023-10-17 11:37:54 -0500

    Added code to find side C and output

---
+++ code.py
@@ -0,0 +1,10 @@
+import math
+print("This code solves for side C of a right triangle using the Pythagorean Theorem.")
+#ask input for a & b values
+a = float(input("What is the length of the first side (Side A)?: "))
+b = float(input("What is the length of the first side (Side B)?: "))
+
+#figure out value of C
+c = math.sqrt(a**2+b**2)
+#output value of C
+print ("The length of C is " + str(c))

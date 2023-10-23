6cfbac8a158ad46891319a5b29e0bfc94b5ccd1c
Eamon6 <146465471+Eamon6@users.noreply.github.com>
2023-10-23 12:02:49 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 import math
-print("This code solves for side C of a right triangle using the Pythagorean Theorem.")
+print("This code solves for side C of a right triangle using the Pythagorean Theorem. ")
 #ask input for a & b values
 a = float(input("What is the length of the first side (Side A)?: "))
 b = float(input("What is the length of the first side (Side B)?: "))
